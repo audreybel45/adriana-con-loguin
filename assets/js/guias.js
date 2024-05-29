fc9ef05fbe45b7226eb5ec832c8d44e0df9dcc4c
audreybel45 <161656511+audreybel45@users.noreply.github.com>
2024-05-29 20:37:40 -0300

    cambios en adriana que se pueden perder

--- assets/js/guias.js
+++ assets/js/guias.js
@@ -1,17 +1,47 @@
-import { cargarSuscriptor } from "./persistencia.js";
-// ESCUCHAMOS AL BOTON SUSCRIBIR Y LLAMAMOS A LA FUNCION DE CARGA
-document.getElementById("footer-boton-suscriptor").addEventListener("click", () => {
-    const correo = document.getElementById("footer-text-subscribir").value
-    //console.log("Probando", correo)
-    cargarSuscriptor(correo)
+document.getElementById('filtrar').addEventListener('click', function() {
+  const ubicacion = document.getElementById('ubicacion').value;
+  const servicios = document.getElementById('servicios').value;
+  const precio = document.getElementById('precio').value;
+  const idiomas = document.getElementById('idiomas').value;
+
+  const guias = [
+    {
+      nombre: 'Daniela Roman',
+      foto: '../assets/images/guies-pictures/daniela-roman.jpg',
+      contacto: {
+        mail: 'daniela@example.com',
+        telefono: '+543885273263'
+      },
+      ubicacion: 'buenos-aires',
+      servicios: ['recepcion', 'itinerario'],
+      precio: 'medio',
+      idiomas: ['espanol', 'ingles']
+    },
+    // Agrega más guías aquí
+  ];
+
+  const guiasFiltradas = guias.filter(guia => {
+    return (ubicacion === 'todas' || guia.ubicacion === ubicacion) &&
+           (servicios === 'todos' || guia.servicios.includes(servicios)) &&
+           (precio === 'todos' || guia.precio === precio) &&
+           (idiomas === 'todos' || guia.idiomas.includes(idiomas));
   });
-  // ESCUCHAMOS AL INPUT POR SI ALGUINE PRECIONA ENTER QUE EJECUTE LA FUNCION DE CARGA
-  document.getElementById("footer-text-subscribir").addEventListener("keydown", function (event) {
-    // Verificar si la tecla presionada es Enter
-    if (event.key === "Enter") {
-      // Detener la propagación del evento para evitar el envío del formulario
-      event.preventDefault();
-      const correo = document.getElementById("footer-text-subscribir").value
-      cargarSuscriptor(correo);
-    }
+
+  const contenedorGuias = document.getElementById('contenedor-guias');
+  contenedorGuias.innerHTML = '';
+
+  guiasFiltradas.forEach(guia => {
+    const guiaDiv = document.createElement('div');
+    guiaDiv.classList.add('guia');
+    guiaDiv.innerHTML = `
+      <img src="${guia.foto}" alt="imagen-${guia.nombre}">
+      <h3>${guia.nombre}</h3>
+      <p>Idiomas: ${guia.idiomas.join(', ')}</p>
+      <p>Servicios: ${guia.servicios.join(', ')}</p>
+      <p>Contacto: <a href="mailto:${guia.contacto.mail}">${guia.contacto.mail}</a>, ${guia.contacto.telefono}</p>
+      <a href="#">Ver perfil</a>
+      <a href="#">Enviar mensaje</a>
+    `;
+    contenedorGuias.appendChild(guiaDiv);
   });
+});
